1a099e789a1558a4c0367dabac0efccaa68387b3
lingyunxiao2 <lingyunxiao2@yy.com>
2022-03-10 10:54:55 +0800

    实现半途放弃退出页面的清理操作

--- app/src/main/java/com/lingyunxiao/gifski/activity/MainActivity.kt
+++ app/src/main/java/com/lingyunxiao/gifski/activity/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.lingyunxiao.gifski.activity
 
-import android.content.Context
-import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
@@ -9,8 +7,6 @@ import android.os.Looper
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.lingyunxiao.gifski.GifskiJniApi
@@ -20,43 +16,38 @@ import com.lingyunxiao.gifski.InstanceKeeper
 import com.lingyunxiao.gifski.ProgressCallback
 import com.lingyunxiao.gifski.R
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val taskKey = 4234
     private val frameCount = 317
     private lateinit var outputFilePath: String
+    private var isTerminated = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (allPermissionsGranted()) {
-            Thread(Runnable {
-                setupGifski()
-                val filesDir = getExternalFilesDir(null)!!
-                outputFilePath = "$filesDir/4234_png/output.gif"
-                val result = process()
-                val resultStr = parseGifskiResult(result)
-                MLog.info(TAG, "finish result means:$resultStr")
-                Handler(Looper.getMainLooper()).post {
-                    txv_result.text = "已完成：$resultStr"
-                    btn_abort.visibility = View.GONE
-                    if (result == 0) {
-                        gif_preview.visibility = View.VISIBLE
-                        Glide.with(this)
-                            .load(outputFilePath)
-                            .transition(withCrossFade())
-                            .into(gif_preview)
-                    }
+        Thread(Runnable {
+            setupGifski()
+            val filesDir = getExternalFilesDir(null)!!
+            outputFilePath = "$filesDir/4234_png/output.gif"
+            val result = process()
+            val resultStr = parseGifskiResult(result)
+            MLog.info(TAG, "finish result means:$resultStr")
+            Handler(Looper.getMainLooper()).post {
+                txv_result.text = "已结束：$resultStr"
+                btn_abort.visibility = View.GONE
+                if (result == 0) {
+                    gif_preview.visibility = View.VISIBLE
+                    Glide.with(this)
+                        .load(outputFilePath)
+                        .transition(withCrossFade())
+                        .into(gif_preview)
                 }
-            }).start()
-
-            btn_abort.setOnClickListener {
-                GifskiJniApi.abort(taskKey)
             }
-        } else {
-            getRuntimePermissions()
+        }).start()
+        btn_abort.setOnClickListener {
+            GifskiJniApi.abort(taskKey)
         }
     }
 
@@ -106,7 +97,6 @@ class MainActivity : AppCompatActivity() {
         val frameList = FrameListBuilder.build()
         MLog.info(TAG, "frame size:${frameList.size}")
         for ((index, frame) in frameList.withIndex()) {
-            // TODO : 实现半途放弃退出页面，确认 gifski 内部会 abort
             val bitmap = BitmapFactory.decodeFile(frame.file.absolutePath)
             // Presentation timestamp (PTS) is time in seconds, since start of the file, when this frame is to be displayed
             val pts = frame.timestampUs / 1000f / 1000.toDouble()
@@ -118,6 +108,7 @@ class MainActivity : AppCompatActivity() {
 //            MLog.info(TAG, "rowBytes:${bitmap.rowBytes}")
 //            val result = GifskiJniApi.addFrameRgb(gifskiNativeObj, bitmap, index, targetWidth, targetHeight, bitmap.rowBytes, 5)
 //            val result = GifskiJniApi.addFrameARgb(gifskiNativeObj, bitmap, index, targetWidth, targetHeight, bitmap.rowBytes, 5)
+            if (isTerminated) return
             if (result != 0) {
                 MLog.info(TAG, "push frame result means:${parseGifskiResult(result)}")
                 return
@@ -126,60 +117,20 @@ class MainActivity : AppCompatActivity() {
         MLog.info(TAG, "done to push ${frameList.size} frames")
     }
 
-    private fun getRequiredPermissions(): Array<String?> {
-        return try {
-            val info = this.packageManager
-                .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
-            val ps = info.requestedPermissions
-            if (ps != null && ps.isNotEmpty()) {
-                ps
-            } else {
-                arrayOfNulls(0)
-            }
-        } catch (e: Exception) {
-            arrayOfNulls(0)
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        GifskiJniApi.abort(taskKey)
+        isTerminated = true
+        truncateGifski()
     }
 
-    private fun allPermissionsGranted(): Boolean {
-        for (permission in getRequiredPermissions()) {
-            permission?.let {
-                if (!isPermissionGranted(this, it)) {
-                    return false
-                }
-            }
-        }
-        return true
-    }
-
-    private fun getRuntimePermissions() {
-        val allNeededPermissions = ArrayList<String>()
-        for (permission in getRequiredPermissions()) {
-            permission?.let {
-                if (!isPermissionGranted(this, it)) {
-                    allNeededPermissions.add(permission)
-                }
-            }
-        }
-
-        if (allNeededPermissions.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                this, allNeededPermissions.toTypedArray(), PERMISSION_REQUESTS
-            )
-        }
-    }
-
-    private fun isPermissionGranted(context: Context, permission: String): Boolean {
-        if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED) {
-            MLog.info(TAG, "Permission granted:$permission")
-            return true
-        }
-        MLog.info(TAG, "Permission NOT granted:$permission")
-        return false
+    private fun truncateGifski() {
+        InstanceKeeper.logger = null
+        InstanceKeeper.progressCallback = null
+        MLog.info(TAG, "truncate:${InstanceKeeper.progressCallback}")
     }
 
     companion object {
         private const val TAG = "MainPage"
-        private const val PERMISSION_REQUESTS = 1
     }
 }
---
+++ app/src/main/java/com/lingyunxiao/gifski/activity/SplashActivity.kt
@@ -0,0 +1,96 @@
+package com.lingyunxiao.gifski.activity
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.lingyunxiao.gifski.R
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        initView()
+        getRuntimePermissions()
+    }
+
+    private fun initView() {
+        if (allPermissionsGranted()) {
+            txv_permgrant.text = "已授权"
+            btn_opt.text = "去操作"
+            btn_opt.setOnClickListener {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        } else {
+            txv_permgrant.text = "需要授予存储权限"
+            btn_opt.text = "去授权"
+            btn_opt.setOnClickListener {
+                getRuntimePermissions()
+            }
+        }
+    }
+
+    private fun getRequiredPermissions(): Array<String?> {
+        return try {
+            val info = this.packageManager
+                .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
+            val ps = info.requestedPermissions
+            if (ps != null && ps.isNotEmpty()) {
+                ps
+            } else {
+                arrayOfNulls(0)
+            }
+        } catch (e: Exception) {
+            arrayOfNulls(0)
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in getRequiredPermissions()) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun getRuntimePermissions() {
+        val allNeededPermissions = ArrayList<String>()
+        for (permission in getRequiredPermissions()) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    allNeededPermissions.add(permission)
+                }
+            }
+        }
+
+        if (allNeededPermissions.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this, allNeededPermissions.toTypedArray(), PERMISSION_REQUESTS
+            )
+        }
+    }
+
+    private fun isPermissionGranted(context: Context, permission: String): Boolean {
+        if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED) {
+            MLog.info(TAG, "Permission granted:$permission")
+            return true
+        }
+        MLog.info(TAG, "Permission NOT granted:$permission")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "SplashPage"
+        private const val PERMISSION_REQUESTS = 1
+    }
+}
